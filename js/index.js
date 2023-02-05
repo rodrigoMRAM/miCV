5ff48729ad34a91794511ef12e43abac64e29003
rodrigoMRAM <rodrigomacielth@gmail.com>
2023-02-05 20:05:37 -0300

    Agregue nuevas imagenes y la parte de redes y mas

--- js/index.js
+++ js/index.js
@@ -3,14 +3,24 @@ const ocultar = document.querySelector(".ocultar");
 const hide = document.querySelector(".hide");
 const experiencia = document.querySelector(".experiencia");
 const principal= document.querySelector(".principal");
+const contacto = document.querySelector(".contacto");
+const hidecontacto = document.querySelector(".hidecontacto");
 
 ocultar.addEventListener("click", ()=>{ 
     principal.classList.add("desactivado");
     experiencia.classList.remove("desactivado");
+    contacto.classList.add("desactivado")
 })
 
 hide.addEventListener("click",()=>{
     principal.classList.remove("desactivado");
+    contacto.classList.add("desactivado")
     experiencia.classList.add("desactivado")
 })
 
+
+hidecontacto.addEventListener("click",()=>{
+    principal.classList.add("desactivado")
+    experiencia.classList.add("desactivado")
+    contacto.classList.remove("desactivado")
+})
